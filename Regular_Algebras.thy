45973dab3b2be0494b7f9db5a0d4729e1f143dd5
Hongjian Jiang <1034060866@qq.com>
2023-09-28 14:48:29 +0200

    Update Regular_Algebras.thy

--- Regular_Algebras.thy
+++ Regular_Algebras.thy
@@ -1296,22 +1296,22 @@ notation
   inf  (infixl "\<sqinter>" 70) and
   sup  (infixl "\<squnion>" 65)
 
+class alp =
+  fixes alp  :: "'a set" ("\<bbbD>")
 
-class var =
-  fixes var  :: "'a set" ("vs")
-
-class antimirow_base = star_dioid + inter_ord + var + boolean_algebra +
+class antimirow_base = star_dioid + inter_ord + alp + boolean_algebra +
+  assumes A12: "(1 \<^bsup>& a = 0) \<and> (a = b \<cdot> a + c) \<Longrightarrow> a = b\<^sup>\<star> \<cdot> c"
   assumes A13: "1 \<^bsup>& (a \<cdot> b) = 1 \<^bsup>& a \<^bsup>& b"                
   assumes A14: "1 \<^bsup>& a\<^sup>\<star> = 1"
   assumes A15: "1 \<^bsup>& a = 0"
-  assumes A16: "x \<in> vs \<Longrightarrow> 0 \<^bsup>& x = 0"
+  assumes A16: "x \<in> \<bbbD> \<Longrightarrow> 0 \<^bsup>& x = 0"
   assumes A17: "a \<^bsup>& a = a"
   assumes A18: "a \<^bsup>& b = b \<^bsup>& a"
   assumes A19: "a \<^bsup>& (b \<^bsup>& c) = a \<^bsup>& b \<^bsup>& c"
   assumes A20: "a \<^bsup>& (b + c) = a \<^bsup>& b + a \<^bsup>& c"
   assumes A21: "a + (a \<^bsup>& b) = a"
-  assumes A22: "x \<in> vs \<and> y \<in> vs \<Longrightarrow> (x \<cdot> a) \<^bsup>& (y \<cdot> b) = (x \<^bsup>& y) \<cdot> (a \<^bsup>& b)"
-  assumes A23: "x \<in> vs \<and> y \<in> vs \<Longrightarrow> (a \<cdot> x) \<^bsup>& (b \<cdot> y) = (a \<^bsup>& b) \<cdot> (x \<^bsup>& y)"
+  assumes A22: "x \<in> \<bbbD> \<and> y \<in> vs \<Longrightarrow> (x \<cdot> a) \<^bsup>& (y \<cdot> b) = (x \<^bsup>& y) \<cdot> (a \<^bsup>& b)"
+  assumes A23: "x \<in> \<bbbD> \<and> y \<in> vs \<Longrightarrow> (a \<cdot> x) \<^bsup>& (b \<cdot> y) = (a \<^bsup>& b) \<cdot> (x \<^bsup>& y)"
   assumes A24: "a \<^bsup>& b = a \<sqinter> b"
   assumes A25: "a + b = a \<squnion> b"
 
