bcc240b88f6e626c617ddb9cfdd1a4f35135c7dc
Hongjian Jiang <1034060866@qq.com>
2024-01-09 15:14:12 +0100

    Update Sequent.thy

--- Sequent.thy
+++ Sequent.thy
@@ -10,11 +10,13 @@ section \<open>Terms and formulae\<close>
 text \<open>
 \label{sec:terms} The datatypes of terms and formulae are defined as follows:
 \<close>
-datatype ('f) tm  = Var nat | Fun 'f \<open>nat list\<close>
+datatype ('f) tm  = Var nat | Fresh nat |  Fun 'f \<open>nat list\<close> 
 
 datatype sign = pos | neg
+
 datatype ('f) form = 
     EqAtom sign "'f tm" "'f tm"
+  | EqFresh sign "'f tm" "'f tm"
   | ConcEq "'f tm" "'f tm" "'f tm"
   | Member sign "'f tm" "char BA rexp"
   | Dis "'f form" "'f form"                     
@@ -23,38 +25,37 @@ datatype ('f) form =
   | FF
   | TT
 
-subsection \<open>Parameters\<close>
+subsection \<open>Fresh term\<close>
 
-primrec params_tm :: "'f tm \<Rightarrow> nat set" where
-  "params_tm (Var n) = {n}"|
-  "params_tm (Fun f ns) = set ns"
+fun fresh_var :: "nat => 'f tm => bool" where
+  "fresh_var x (Var y) = (x \<noteq> y)" |
+  "fresh_var x (Fun f xs) = (list_all (\<lambda>a. a = x) xs)" 
 
-primrec params_fm :: "'f form \<Rightarrow> nat set" where
-  "params_fm (EqAtom s f1 f2) = (params_tm f1 \<union> params_tm f2)"|
-  "params_fm (ConcEq f1 f2 f3) = (params_tm f1 \<union> params_tm f2 \<union> params_tm f3)"|
-  "params_fm (Member s f r) = (params_tm f)"|
-  "params_fm (Dis f1 f2) = (params_fm f1 \<union> params_fm f2)"|
-  "params_fm (Con f1 f2) = (params_fm f1 \<union> params_fm f2)"|
-  "params_fm (Neg f) = (params_fm f)"|
-  "params_fm (FF) = {}"|
-  "params_fm TT = {}"
+primrec deX :: "'f tm => nat list" where 
+  "deX (Var n) = [n] "|
+  "deX (Fun f xs) = xs"
 
-definition fresh :: "'f tm \<Rightarrow> 'f tm \<Rightarrow> nat list \<Rightarrow> bool" where 
-"fresh x y xs = (\<not> params_tm y \<subseteq> (params_tm x \<union> set xs))"
+definition
+  freshVar :: "string tm list => string tm" where
+  "freshVar vs = Var (LEAST n. n \<notin> set (concat (map deX vs)))"
 
-abbreviation \<open>params S \<equiv> \<Union>p \<in> S. params_fm p\<close>
+lemma "freshVar [Var (0::nat), Var 1, Var 3] = Var 2"
+  apply (auto simp: freshVar_def Least_def)
+  using Zero_not_Suc bot_nat_0.extremum by fastforce
 
 subsection \<open>Semantics of term and form\<close>
 
 primrec semantics_tm :: "(nat \<Rightarrow> string) \<Rightarrow> ('f \<Rightarrow> string list \<Rightarrow> string) \<Rightarrow> 'f tm \<Rightarrow> string" (\<open>\<lparr>_, _\<rparr>\<close>) where
-  \<open>\<lparr>E, F\<rparr> (Var n) = E n\<close>
-| \<open>\<lparr>E, F\<rparr> (Fun f ts) = F f (map E ts)\<close>
+  \<open>\<lparr>E, F\<rparr> (Var n) = E n\<close> |
+  \<open>\<lparr>E, F\<rparr> (Fresh n) = E n\<close> |
+  \<open>\<lparr>E, F\<rparr> (Fun f ts) = F f (map E ts)\<close>
 
 fun concat_str :: "string \<Rightarrow> string list \<Rightarrow> string" where
-"concat_str s ls = (if s = ''concat'' then List.concat ls else [])"
+  "concat_str s ls = (if s = ''concat'' then List.concat ls else [])"
 
 primrec semantics_fm (\<open>\<lbrakk>_, _\<rbrakk>\<close>) where
   \<open>\<lbrakk>E, F\<rbrakk> (EqAtom s x y) = (if s = pos then \<lparr>E, F\<rparr> x = \<lparr>E, F\<rparr> y else \<lparr>E, F\<rparr> x \<noteq> \<lparr>E, F\<rparr> y)\<close> 
+| \<open>\<lbrakk>E, F\<rbrakk> (EqFresh s x y) = True\<close> 
 | \<open>\<lbrakk>E, F\<rbrakk> (ConcEq z x y) = (\<lparr>E, F\<rparr> z = \<lparr>E, F\<rparr> x @ \<lparr>E, F\<rparr> y)\<close> 
 | \<open>\<lbrakk>E, F\<rbrakk> (Member s x r) = (if s = pos then \<lparr>E, F\<rparr> x \<in> lang r else \<lparr>E, F\<rparr> x \<notin> lang r)\<close> 
 | \<open>\<lbrakk>E, F\<rbrakk> (Dis x y) = (\<lbrakk>E, F\<rbrakk> x \<or> \<lbrakk>E, F\<rbrakk> y)\<close> 
@@ -70,11 +71,11 @@ fun is_Member :: "'f form \<Rightarrow> bool" where
   "is_Member _ = False"
 
 fun variable_in_member :: "'f form \<Rightarrow> 'f tm option" where 
-"variable_in_member (Member s x r) = Some x"|
-"variable_in_member _ = None"
+  "variable_in_member (Member s x r) = Some x"|
+  "variable_in_member _ = None"
 
 fun rexp_in_member :: "'f form \<Rightarrow> char BA rexp  " where 
-"rexp_in_member (Member s x r) = r"
+  "rexp_in_member (Member s x r) = r"
 
 definition "distinct_variable  ls = distinct (map (variable_in_member) ls)"
 
@@ -117,14 +118,14 @@ inductive One_SC :: \<open>string form list  \<Rightarrow> bool\<close> (\<open>
 | AlphaNegAnd:   \<open>\<stileturn> Neg A # \<Gamma> \<Longrightarrow>  \<stileturn> Neg B # \<Gamma> \<Longrightarrow> \<stileturn> Neg (Con A B) # \<Gamma>\<close>
 | AlphaNegNeg:   \<open>\<stileturn> A# \<Gamma> \<Longrightarrow> \<stileturn> Neg (Neg A) # \<Gamma>\<close>
 | NotMember:     \<open>regexp_compl e ec \<Longrightarrow> \<stileturn> (Member pos x ec) # \<Gamma> \<Longrightarrow> \<stileturn> (Member neg x e) # \<Gamma>\<close>
-| NotEq:         \<open>\<stileturn> [EqAtom neg x y,  EqAtom pos y (Fun f xs), \<Gamma>] \<Longrightarrow> \<stileturn> [EqAtom neg x (Fun f xs), \<Gamma>]\<close>
+| NotEq:         \<open>y = freshVar ([x,(Fun f xs)])  \<Longrightarrow> \<stileturn> [EqFresh neg x y,  EqFresh pos y (Fun f xs), \<Gamma>] \<Longrightarrow> \<stileturn> [EqAtom neg x (Fun f xs), \<Gamma>]\<close>
 | Cut:           \<open>regexp_compl e ec \<Longrightarrow> \<stileturn> Member pos x e # \<Gamma> \<Longrightarrow>  \<stileturn> Member pos x ec # \<Gamma> \<Longrightarrow>  \<stileturn> \<Gamma>\<close>
 | EqProp:        \<open>\<stileturn> Member pos x e # EqAtom pos x y # Member pos y e # \<Gamma> \<Longrightarrow> \<stileturn> Member pos x e # EqAtom pos x y # \<Gamma>\<close>
 | NeqSubsume:    \<open>regexp_empty e1 e2 \<Longrightarrow> \<stileturn> Member pos x e1 # Member pos y e2 # \<Gamma> \<Longrightarrow> \<stileturn> Member pos x e1 # EqAtom neg x y # Member pos y e2 # \<Gamma>\<close>
 | EqPropElim:    \<open>is_singleton (lang e) \<Longrightarrow> \<stileturn> Member pos x e # Member pos y e # \<Gamma>\<Longrightarrow> \<stileturn> Member pos x e # (EqAtom pos x y) # \<Gamma>\<close>
 | NeqPropElim:   \<open>is_singleton (lang e) \<Longrightarrow> regexp_compl e ec \<Longrightarrow> \<stileturn> (Member pos x e) # (Member pos y ec) # \<Gamma> \<Longrightarrow>  
                  \<stileturn> (Member pos x e) # (EqAtom neg x y) # \<Gamma>\<close>
-| Close:         \<open>empty_intersection_set rs \<Longrightarrow> \<stileturn> [FF] \<Longrightarrow> \<stileturn> map (\<lambda>r. Member pos x r) rs\<close> 
+| Close:         \<open>empty_intersection_set rs \<Longrightarrow> \<stileturn> map (\<lambda>r. Member pos x r) rs\<close> 
 | Subsume:       \<open>subset_intersect_set e fs \<Longrightarrow> \<stileturn> (map (\<lambda>r. Member pos x r) fs) @ \<Gamma> \<Longrightarrow> \<stileturn> Member pos x e # (map (\<lambda>r. Member pos x r) fs) @ \<Gamma>\<close> 
 | Intersect:     \<open>eq_len_intersect e fs \<Longrightarrow> \<stileturn> Member pos x e # \<Gamma>  \<Longrightarrow>  \<stileturn> (map (\<lambda>r. Member pos x r) (fs)) @ \<Gamma>\<close> 
 | Fwd_PropConc:  \<open>con_fwd_prop e e1 e2 \<Longrightarrow> \<stileturn> [(Member pos x e), (EqAtom pos x (Fun ''concat'' [x1,x2])), (Member pos (Var x1) e1), (Member pos (Var x2) e2)] @ \<Gamma>
@@ -134,63 +135,25 @@ inductive One_SC :: \<open>string form list  \<Rightarrow> bool\<close> (\<open>
 (*| Bwd_PropConc:  \<open>con_bwd_prop e = es \<Longrightarrow> \<stileturn> ((\<lambda>r. [Member x e, EqAtom x (Fun ''concat'' [x1,x2]), Member (Var x1) (fst r), Member (Var x2) (snd r)] @ \<Gamma>) ` es) \<Longrightarrow> 
                  \<stileturn> {[Member x e, EqAtom x (Fun ''concat'' [x1,x2])] @ \<Gamma>}\<close>*)
 | Order:         \<open>\<stileturn> G \<Longrightarrow> set G = set G' \<Longrightarrow> \<stileturn> G'\<close>
-(*| Basic:        \<open>exists_solution G  \<Longrightarrow> \<stileturn> [FF]\<Longrightarrow> \<stileturn> G\<close>*)
+| Basic:        \<open> \<stileturn> [A,Neg A, G]\<close>
 
 declare One_SC.intros [intro]
 
-(*lemma "\<stileturn> {((map (\<lambda>r. Member pos x r) [Pred (Atom CHR ''b''), Pred (Atom CHR ''a'')] @ []))}"
-  apply (rule Close)
-  subgoal apply auto done
-  apply (rule Basic) 
-  done
-
-
-lemma "\<stileturn> {((map (\<lambda>r. Member pos x r) [Pred (Atom CHR ''b''), Pred (Atom CHR ''a'')] @ []))}"
-  apply (rule Close)
-  apply (auto simp:distinct_variable_def single_word_def)
-  done
-
-lemma "\<stileturn> {[Member (Var 1) (Pred (Atom CHR ''b''))]}"
-  apply (rule Basic1)
-  apply (auto simp:distinct_variable_def single_word_def)
-  done
-*)
 subsection \<open>Soundness\<close>
 
-lemma SC_soundness: \<open>\<stileturn> G \<Longrightarrow> (\<forall>p \<in> set G.  \<lbrakk>E, concat_str\<rbrakk> p)\<close>
+lemma SC_soundness: \<open>\<stileturn> G \<Longrightarrow> (\<exists>p \<in> set G. \<not> \<lbrakk>E, concat_str\<rbrakk> p)\<close>
 proof (induct G rule: One_SC.induct)
-  case (NeqSubsume e1 e2 x y \<Gamma>)
-  then show ?case apply auto done
-next
-  case (EqPropElim e x y \<Gamma>)
-  then show ?case apply auto 
-    by (metis is_singletonE singletonD)
-next
-  case (NeqPropElim e ec x y \<Gamma>)
-  then show ?case apply (auto simp: is_singleton_def)  done 
-next
-  case (Subsume e fs x \<Gamma>)
-  then show ?case apply auto 
-    by (smt (verit, ccfv_SIG) INT_I image_subset_iff semantics_fm.simps(3) subsetD sup.cobounded1)
-next
   case (Intersect e fs x \<Gamma>)
   then show ?case  apply auto 
-    by blast
-next
-  case (Fwd_PropConc e e1 e2 x x1 x2 \<Gamma>)
-  then show ?case apply (auto simp:c_prod_def times_list_def)  done
-next
-  case (Fwd_ElimConc e e1 e2 x x1 x2 \<Gamma>)
-  then show ?case apply (auto simp:c_prod_def times_list_def) 
-    by (smt (verit) is_singleton_def mem_Collect_eq singleton_iff) 
-qed auto
+    by (smt (verit) INT_I image_subset_iff semantics_fm.simps(4) sup.cobounded1)
+qed (auto simp:l_prod_elim is_singleton_def)
 
 (*next
   case (Bwd_PropConc e es x1 x2 x \<Gamma>)
   then show ?case apply (auto simp:c_prod_def times_list_def) sorry*)
 
 definition SC_proof :: \<open>string form list \<Rightarrow> string form \<Rightarrow> bool\<close> where
-  \<open>SC_proof ps p \<equiv> (\<stileturn> p # ps)\<close>
+  \<open>SC_proof ps p \<equiv> (\<stileturn> Neg p # ps)\<close>
 
 theorem sc_soundness:
   \<open>SC_proof ps p \<Longrightarrow> list_all \<lbrakk>E, concat_str\<rbrakk> ps \<Longrightarrow> \<lbrakk>E, concat_str\<rbrakk> p\<close>
@@ -198,13 +161,6 @@ theorem sc_soundness:
   by fastforce
 
 subsection \<open>Consistent sets\<close>
-
-text \<open>
-\label{sec:consistent-sets}
-In this section, we describe an abstract criterion for consistent sets.
-A set of sets of formulae is called a {\em consistency property}, if the
-following holds:
-\<close>
 
 definition consistency :: \<open>string form set set \<Rightarrow> bool\<close> where
   \<open>consistency C = (\<forall>S. S \<in> C \<longrightarrow> FF \<notin> S \<and> 
